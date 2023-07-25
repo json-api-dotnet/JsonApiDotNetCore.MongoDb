eb7247d986134293cf75cae757815c5dcc305ed6
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2023-07-25 22:01:17 +0200

    Add missing controller annotations in tests

--- test/JsonApiDotNetCoreMongoDbTests/IntegrationTests/ReadWrite/WorkTag.cs
+++ test/JsonApiDotNetCoreMongoDbTests/IntegrationTests/ReadWrite/WorkTag.cs
@@ -6,6 +6,7 @@ using MongoDB.Bson.Serialization.Attributes;
 namespace JsonApiDotNetCoreMongoDbTests.IntegrationTests.ReadWrite;
 
 [UsedImplicitly(ImplicitUseTargetFlags.Members)]
+[Resource(ControllerNamespace = "JsonApiDotNetCoreMongoDbTests.IntegrationTests.ReadWrite")]
 public sealed class WorkTag : HexStringMongoIdentifiable
 {
     [Attr]
