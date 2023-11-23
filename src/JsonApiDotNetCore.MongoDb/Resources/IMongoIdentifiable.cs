ed33dfa4f56b8d665bd9be6482a0d01409973984
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2023-11-23 02:55:16 +0100

    Resharper: remove redundant body

--- src/JsonApiDotNetCore.MongoDb/Resources/IMongoIdentifiable.cs
+++ src/JsonApiDotNetCore.MongoDb/Resources/IMongoIdentifiable.cs
@@ -5,6 +5,4 @@ namespace JsonApiDotNetCore.MongoDb.Resources;
 /// <summary>
 /// Marker interface to indicate a resource that is stored in MongoDB.
 /// </summary>
-public interface IMongoIdentifiable : IIdentifiable<string?>
-{
-}
+public interface IMongoIdentifiable : IIdentifiable<string?>;
